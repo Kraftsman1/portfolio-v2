4029a95d4070611cf6738c8ac4443fb4b2f2881d
Asigri Shamsu-Deen Al-Heyr <deen@coldsis.com>
2023-05-29 18:30:30 +0000

    setup portfolio v2

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+import { defineNuxtConfig } from "nuxt/config";
+
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Kraftman',
+      titleTemplate: 'Asigri Shamsu-Deen Al-Heyr | Portfolio',
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'MisFyts 3D' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+  },
+
+  modules: [],
+  plugins: [],
+  ssr: false,
+
+  devtools: {
+    enabled: true,
+  },
+});
